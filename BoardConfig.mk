320ee205f18e7dddf8bbb741fcd5acdac6dd0778
zatvio <lordkazuya1@gmail.com>
2025-01-09 04:57:13 +0000

    Revert "cupida: Remove TARGET_2ND_ARCH_VARIANT"
    This reverts commit 2af49e2f722a0adc9e2a4221fef3a892830eea72.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ DEVICE_PATH := device/oplus/cupida
 # Inherit from oplus mt6893-common
 include device/oplus/mt6893-common/BoardConfigCommon.mk
 
+
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U
 
 # Assertation
